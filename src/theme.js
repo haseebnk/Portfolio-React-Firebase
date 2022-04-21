07dac6320a95bf7478ea4407797fa8d8f7883799
unknown <haseebnk37@gmail.com>
2022-04-21 16:17:42 +0500

    done

---
+++ src/theme.js
@@ -0,0 +1,46 @@
+// Theme Presets
+
+const lightTheme = {
+  name: "light",
+  body: "#FFEEEE",
+  text: "#343434",
+  dark: "#000000",
+  secondaryText: "#7F8DAA",
+  accentColor: "#E3405F",
+  accentBright: "#E3405F",
+  projectCard: "#DCE4F2",
+  skinColor: "#F7B799",
+  skinColor2: "#FCB696",
+  imageDark: "#dce4f2",
+  imageClothes: "#dce4f2",
+  avatarMisc: "#e9ecf2",
+  avatarShoes: "#ccd2e3",
+};
+
+const darkTheme = {
+  name: "dark",
+  body: "#040303",
+  text: "#FFFFFF",
+  dark: "#000000",
+  secondaryText: "#8D8D8D",
+  accentColor: "#E3405F",
+  accentBright: "#E3405F",
+  projectCard: "#292A2D",
+  skinColor: "#F7B799",
+  skinColor2: "#FCB696",
+  imageDark: "#292A2D",
+  imageClothes: "#000000",
+  avatarMisc: "#212121",
+  avatarShoes: "red",
+
+  // text: "#5D2A42",
+  highlight: "#FFF9EC",
+  dark: "#E3405F",
+  secondaryText: "#8D697A",
+  imageHighlight: "#E29F95",
+  compImgHighlight: "#E6E6E6",
+  jacketColor: "#FB6376",
+  headerColor: "#E29F9577",
+};
+
+export const themes = { light: lightTheme, dark: darkTheme };
